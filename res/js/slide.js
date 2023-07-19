96b90d2a3fdd0df1ac1705e793359c324497714a
navilindo <hassainjr@gmail.com>
2023-07-20 00:06:28 +0300

    chanhges

--- res/js/slide.js
+++ res/js/slide.js
@@ -1,4 +1,23 @@
 const slide = () => {
+
+    const images = [
+        '../res/images/imgs/tea-farm.png',
+        '../res/images/imgs/beans.png',
+        '../res/images/imgs/sunflower.png'
+    ];
+
+    const currentImage = -1;
+
+    const nextImage = currentImage + 1;
+    if (nextImage >= images.length) {
+        nextImage = 0;
+        
+    }
+
+    const imageUrl = images[nextImage];
+    document.getElementById("background").style.backgroundImage = `url(${imageUrl})`;
+
+
     const width = document.getElementById("background").offsetWidth;
     const left = document.getElementById("background").style.left;
   
@@ -9,5 +28,5 @@ const slide = () => {
     }
   };
   
-  setInterval(slide, 3000);
+  setInterval(slide, 1000);
   
